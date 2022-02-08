09c707ed185904e8fd449ad75f7de1b1c4e418f6
brunnermanu <brunner.manu@hotmail.com>
2022-02-08 16:33:45 +0100

    remove email from list item

--- src/listItem.tsx
+++ src/listItem.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-const ListItem = ({ name, email}: { name:string; email:string}): JSX.Element => {
+const ListItem = (name :string): JSX.Element => {
   return (
     <div className="border border-gray-300 px-3 py-2 hover:cursor-pointer hover:bg-gray-300">
       <h2>{name}</h2>
-      <p>{email}</p>
     </div>
   )
 }
