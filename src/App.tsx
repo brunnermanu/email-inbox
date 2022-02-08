206fc5ac9940829a884133812871c28c0a40a75e
brunnermanu <brunner.manu@hotmail.com>
2022-02-08 16:30:23 +0100

    fetch data, use components and add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import ListItem from "./listItem";
+import InfoBox from "./InfoBox";
+import './index.css';
+
 
 function App() {
+
+  const [userData , setUserData] = useState({
+    posts: [],
+    dataIsLoaded: false
+  })
+
+  type Post = {
+    id: number,
+    author: string,
+    width: number,
+    height: number,
+    url: string,
+    download_url: string
+  }
+  const [currentPost, setCurrentPost] = useState({id:0, author:"",download_url:""})
+
+  const getPosts = () => {
+    fetch(
+      "https://picsum.photos/v2/list")
+      .then((res) => res.json())
+      .then((json) => {
+        setUserData({
+          posts: json,
+          dataIsLoaded: true
+        });
+      })
+  }
+
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
+const handleClick = (id:number) => {
+  userData.posts.find((post:Post) => post.id === id ? setCurrentPost(post) : null)
+}
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="grid grid-cols-3">
+      <div className="col-span-1">
+        {userData.posts.map((post:Post) => {
+          return (
+            <div key={post.id} onClick={() => handleClick(post?.id)}>
+              <ListItem  name={post?.author} email={post?.download_url} />
+            </div>
+          )
+        })}
+      </div>
+      { currentPost ?
+        <div key={currentPost.id} className="col-span-2">
+          <InfoBox  author={currentPost.author} download_url={currentPost.download_url}/>
+        </div> : null
+      }
+    </div>
+
   );
 }
 
