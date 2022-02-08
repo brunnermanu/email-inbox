4a3db87c031734161b664aeca1c90677259956a4
brunnermanu <brunner.manu@hotmail.com>
2022-02-08 16:49:39 +0100

    change attributes on listitem

--- src/listItem.tsx
+++ src/listItem.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ListItem = (name :string): JSX.Element => {
+const ListItem = ({author}:{author: string}): JSX.Element => {
   return (
     <div className="border border-gray-300 px-3 py-2 hover:cursor-pointer hover:bg-gray-300">
-      <h2>{name}</h2>
+      <h2>{author}</h2>
     </div>
   )
 }
