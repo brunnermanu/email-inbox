fc33988a1278784a7153cb8b17ca957059fdde25
manuelabrunner <manuela.brunner@simplificator.com>
2022-02-11 09:50:54 +0100

    improvehandleClick, improve mobile view and a few small styling improvements

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import InfoBox from "./InfoBox";
 import './index.css';
 
 export interface Post {
-  id: number,
+  id: number | undefined,
   author: string,
   url: string,
   download_url: string,
@@ -19,7 +19,7 @@ function App() {
   })
 
   const [visible, setVisible] = useState(false)
-  const [currentPost, setCurrentPost] = useState<Post>({id: -1, author: "", download_url: "", url: ""})
+  const [currentPost, setCurrentPost] = useState<Post>({id: undefined, author: "", download_url: "", url: ""})
 
   const getPosts = () => {
     fetch(
@@ -37,42 +37,45 @@ function App() {
     getPosts()
   }, [])
 
-  const handleClick = (id: number | undefined) => {
-    postData.posts.find((post: Post) => {
-      if (id === currentPost.id) {
-        setVisible(false)
-        setCurrentPost({id: -1, author: "", download_url: "", url: ""})
-      }
-      if (post.id === id) {
-        setCurrentPost(post)
-        setVisible(true)
-      }
-    })
+  const handleClick = (post: Post) => {
+    if (post.id === currentPost.id) {
+      setCurrentPost({id: undefined, author: "", download_url: "", url: ""})
+      setVisible(false)
+    } else {
+      setCurrentPost(post)
+      setVisible(true)
+    }
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3">
-      <div className="col-span-1 overflow-y-scroll h-screen">
-        { postData.dataIsLoaded ?
-          postData.posts.map((post: Post) => {
-            return (
-              <div key={post.id} onClick={() => handleClick(post?.id)}>
-                <ListItem post={post}/>
-              </div>
-              )
-            }) :
-            <div className="w-screen h-screen flex items-center justify-center">
-              <h1 className="text-center">Loading ...</h1>
-            </div>
+    <>
+      { !postData.dataIsLoaded &&
+        <div className="w-screen h-screen flex items-center justify-center">
+          <h1 className="text-center">Loading ...</h1>
+        </div>
+      }
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4">
+        <div className="col-span-1 overflow-y-scroll h-screen">
+          { postData.dataIsLoaded &&
+            postData.posts.map((post: Post) => {
+              return (
+                <div key={post.id} onClick={() => handleClick(post)}>
+                  <ListItem post={post}/>
+                </div>
+                )
+              })
+          }
+        </div>
+        {currentPost &&
+        <div key={currentPost.id}
+             className={visible ?
+               "absolute top-0 left-0 z-10 h-full w-full visible flex flex-col bg-white col-span-1 sm:col-span-2 md:col-span-3 sm:static sm:bg-white sm:h-auto sm:w-full sm:items-center"
+               : "invisible"}>
+          <InfoBox post={currentPost} onClick={() => setVisible(false)}/>
+        </div>
         }
       </div>
-      {currentPost &&
-      <div key={currentPost?.id}
-           className={visible ? "absolute top-0 left-0 z-10 h-screen w-screen visible flex flex-col bg-white sm:col-span-2 sm:static sm:bg-white sm:h-auto sm:w-full sm:items-center" : "invisible"}>
-        <InfoBox post={currentPost} onClick={() => setVisible(false)}/>
-      </div>
-      }
-    </div>
+    </>
   )
 }
 
--- src/InfoBox.tsx
+++ src/InfoBox.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Post } from "./App";
 
-const InfoBox = ({ post, onClick }:{ post:Post, onClick:()=> void }): JSX.Element => {
+const InfoBox = ({ post, onClick }: { post: Post, onClick:() => void }): JSX.Element => {
   return (
-    <div className="px-12 mb-14 fixed flex flex-col items-center">
-      <div className="flex w-full flex-row grid items-start sm:invisible">
+    <div className="px-3 sm:px-12 mb-14 fixed flex flex-col items-center">
+      <div className="flex w-full flex-row items-start sm:invisible">
         <button className="pt-8 text-2xl w-5 font-semibold" onClick={onClick}>x</button>
       </div>
       <h1 className="pb-10 pt-10 sm:pt-0">{post.author}</h1>
-      <img className="max-h-80 md:w-auto rounded-lg" src={post.download_url} alt={post.author}/>
+      <img className="w-auto sm:max-h-96 rounded-lg" src={post.download_url} alt={post.author}/>
       <p className="mt-8 text-gray-400">Download-Url:</p>
-      <a className="mb-4" href={post.download_url} target={"_blank"}>{post.download_url}</a>
+      <a className="mb-4" href={post.download_url} target="_blank" rel="noreferrer">{post.download_url}</a>
       <p className="text-gray-400">Url:</p>
-      <a className="mb-10" href={post.url} target={"_blank"}>{post.url}</a>
+      <a className="mb-10" href={post.url} target="_blank" rel="noreferrer">{post.url}</a>
     </div>
   )
 }
